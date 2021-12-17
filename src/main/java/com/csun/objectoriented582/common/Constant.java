a637a7da1d18bd0356cb81ef969d1eb8fb8c4d87
luhao1861 <luhao7872@gmail.com>
2021-12-17 07:47:30 -0800

    final

--- src/main/java/com/csun/objectoriented582/common/Constant.java
+++ src/main/java/com/csun/objectoriented582/common/Constant.java
@@ -4,5 +4,5 @@ public class Constant {
     public final static Long STATUS_ON = 1L;
     public final static Long STATUS_OFF = 0L;
     public static final String DEFULT_PASSWORD = "123456";
-    public static final String DEFULT_AVATAR = "https://image-1300566513.cos.ap-guangzhou.myqcloud.com/upload/images/5a9f48118166308daba8b6da7e466aab.jpg";
+    public static final String DEFULT_AVATAR = "https://www.kindpng.com/picc/m/78-786678_avatar-hd-png-download.png";
 }
